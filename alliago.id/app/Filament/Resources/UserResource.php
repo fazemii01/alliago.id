0ed39a532c41a0cec506eb57fe28ca9b25a3b288
fazemii01 <bestvatp@gmail.com>
2026-05-30 15:26:47 +0700

    flight

--- alliago.id/app/Filament/Resources/UserResource.php
+++ alliago.id/app/Filament/Resources/UserResource.php
@@ -92,6 +92,12 @@ class UserResource extends Resource
             ])
             ->actions([
                 Tables\Actions\EditAction::make(),
+                Tables\Actions\DeleteAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
             ]);
     }
 
