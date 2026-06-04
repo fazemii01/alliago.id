2a88efe9ec6872e5e615334757d66c889d103ccb
fazemii01 <bestvatp@gmail.com>
2026-06-04 13:54:06 +0700

    6-4 1:35 final

--- alliago.id/resources/views/components/home/bottom-nav.blade.php
+++ alliago.id/resources/views/components/home/bottom-nav.blade.php
@@ -28,11 +28,11 @@
             <span class="text-[10px] font-semibold">Tiket Ferry</span>
         </a>
 
-        <a href="{{ route('pages.faq') }}" class="flex flex-col items-center justify-center gap-1 w-full h-full transition {{ request()->routeIs('pages.faq') ? 'text-[#0361fc]' : 'text-slate-500 hover:text-[#0361fc]' }}">
+        <a href="{{ route('pages.currency_rates') }}" class="flex flex-col items-center justify-center gap-1 w-full h-full transition {{ request()->routeIs('pages.currency_rates') ? 'text-[#0361fc]' : 'text-slate-500 hover:text-[#0361fc]' }}">
             <svg class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="1.5">
-                <path stroke-linecap="round" stroke-linejoin="round" d="M8.228 9c.549-1.165 2.03-2 3.772-2 2.21 0 4 1.343 4 3 0 1.4-1.278 2.575-3.006 2.907-.542.104-.994.54-.994 1.093m0 3h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+                <path stroke-linecap="round" stroke-linejoin="round" d="M12 8c-1.657 0-3 .895-3 2s1.343 2 3 2 3 .895 3 2-1.343 2-3 2m0-8c1.11 0 2.08.402 2.599 1M12 8V7m0 1v8m0 0v1m0-1c-1.11 0-2.08-.402-2.599-1M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
             </svg>
-            <span class="text-[10px] font-semibold">FAQ</span>
+            <span class="text-[10px] font-semibold">{{ __('common.nav_currency_rates') }}</span>
         </a>
     </div>
 </div>
